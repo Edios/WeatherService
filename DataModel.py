038ef810f2c03a4d4484cec634b9cca383ad093a
Miroslaw Lazarewicz <miroslaw.lazarewicz@gmail.com>
2022-03-06 20:20:30 +0100

    DTO CD

--- DataModel.py
+++ DataModel.py
@@ -1,5 +1,21 @@
+from dataclasses import field
+
+
 class Holiday:
     name:str
     date:str
 
-#TODO: Object to hold Holiday
+class TemperatureStatistic:
+    date:str
+    temperature:int
+
+class HolidayTemperatureStatistics:
+    date_from:str
+    date_to:str
+    #Represent Holiday specific day
+    date:str
+    temperatures:list[TemperatureStatistic]=field(init=False)
+
+    def get_temperatures(self):
+
+        pass
